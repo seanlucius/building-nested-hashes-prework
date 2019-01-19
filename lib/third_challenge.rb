5be09f3fbbf36b2dfc64201525b61bf34c3368e2
Sean Lucius <seanlucius22@icloud.com>
2019-01-19 03:15:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
         age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague"
         },
       :hero => {
         #your key/value pairs here
